0b36b96d14dbd098ad4d789381dc9a6b8e731e34
Alex-Shubin <alex.cuo@mail.ru>
2025-02-10 02:15:05 +0300

    6_2 re-structured

--- lesson6/2.py
+++ lesson6/2.py
@@ -12,11 +12,18 @@ user_login = input("Введите логин: ")
 user_pass = input("Введите пароль: ")
 user_age = int(input("Сколько вам лет? "))
 
-if (user_login == "admin" and user_pass == "123456"):
+if ((user_login == "admin" and user_pass == "123456") 
+    or (user_login == "vasya" and user_pass == "vas123" and user_age < 60) 
+    or (user_login == "guest" and user_age > 18)):
     print("доступ разрешен")
-elif (user_login == "vasya" and user_pass == "vas123" and user_age < 60):
-        print("доступ разрешен")
-elif (user_login == "guest" and user_age > 18):
-        print("доступ разрешен")
 else: 
     print("доступ запрещен")
+
+# if (user_login == "admin" and user_pass == "123456"):
+#     print("доступ разрешен")
+# elif (user_login == "vasya" and user_pass == "vas123" and user_age < 60):
+#         print("доступ разрешен")
+# elif (user_login == "guest" and user_age > 18):
+#         print("доступ разрешен")
+# else: 
+#     print("доступ запрещен")

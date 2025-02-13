f3d0a3491326034c9e746f449a0c42fb58e89f66
Alex-Shubin <alex.cuo@mail.ru>
2025-02-13 18:20:36 +0300

    9_4 - вопрос по if в comprehension

--- lesson9/4.py
+++ lesson9/4.py
@@ -11,15 +11,33 @@
 '''
 init_list = [1,2,3,4,5,6,7,8,9]
 
+# [1, 4, 9, 16, 25, 36, 49, 64, 81]
 exp_list = list(map(lambda i: i ** 2, init_list.copy()))
-# print(exp_list) # [1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(exp_list) 
 
+
+# [1, 5, 3, 7, 5, 9, 7, 11, 9]
 sum_list = list(map(lambda i: i + 3 if i % 2 == 0 else i, init_list.copy()))
-# print(sum_list) # [1, 5, 3, 7, 5, 9, 7, 11, 9]
+# print(sum_list) 
 
-# sum_list = list(map(lambda i: i + 3, filter(lambda x: x % 2 == 0, init_list.copy())))
-# print(sum_list) # [5, 7, 9, 11]
 
+# [5, 7, 9, 11]
+# sum_list = (list(map(lambda i: i + 3, 
+#                     filter(lambda x: x % 2 == 0, init_list.copy()))))
+# print(sum_list) 
+
+
+# [3, 4, 9, 8, 15, 12, 21, 16, 27]
 even_odd_list = (list(map(lambda i: i * 2 if i % 2 == 0 
                           else i * 3, init_list.copy())))
-# print(even_odd_list) # [3, 4, 9, 8, 15, 12, 21, 16, 27]
+# print(even_odd_list) 
+
+"""
+почему у меня не получается использовать выражение
+sum_list = list(map(lambda i: i + 3 if i % 2 == 0, init_list.copy()))
+?
+требует else - в решении с ним и сделал
+и почему не требует else в примере из comprehension:
+d = {user['name']:len(user['name']) for user in users3 if user['age']==12}
+print(d)
+"""
